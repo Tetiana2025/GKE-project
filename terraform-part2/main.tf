d69ee965d162cbec8cecd101884b2205e84297d9
Tetiana2025 <tetianabahlai@gmail.com>
2025-06-13 23:26:23 +0000

    added changes in files

--- terraform-part2/main.tf
+++ terraform-part2/main.tf
@@ -1,26 +1,26 @@
 resource "helm_release" "api" {
   name       = "api"
-  chart      = "../helm/api"
+  chart      = "./helm/api"
   namespace  = "default"
   create_namespace = true
   timeout = 600
-  values     = [file("../helm/api/values.yaml")]
+  values     = [file("./helm/api/values.yaml")]
 }
 
 resource "helm_release" "web" {
   name       = "web"
-  chart      = "../helm/web"
+  chart      = "./helm/web"
   namespace  = "default"
   create_namespace = true
   timeout = 600
-  values     = [file("../helm/web/values.yaml")]
+  values     = [file("./helm/web/values.yaml")]
 }
 
 resource "helm_release" "mysql" {
   name       = "mysql"
-  chart      = "../helm/mysql"
+  chart      = "./helm/mysql"
   namespace  = "default"
   create_namespace = true
   timeout = 600
-  values     = [file("../helm/mysql/values.yaml")]
+  values     = [file("./helm/mysql/values.yaml")]
 }
